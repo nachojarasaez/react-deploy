27d1d41fb7b2348d19591a80329d7c5116e9ffa5
nachojarasaez <injara@uc.cl>
2020-05-16 23:56:19 -0400

    Updates

---
+++ precache-manifest.dc6fb4540d2dc88bb3d2a13c5d227208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517a15999032599eaa7e6a5c84573fee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e145801579ec29ab2493",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8607a170ea58e34a9071",
+    "url": "/static/css/main.c1c68314.chunk.css"
+  },
+  {
+    "revision": "e145801579ec29ab2493",
+    "url": "/static/js/2.9927896c.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "/static/js/2.9927896c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8607a170ea58e34a9071",
+    "url": "/static/js/main.f784bcbb.chunk.js"
+  },
+  {
+    "revision": "1a275f40f39acf6840d8",
+    "url": "/static/js/runtime-main.3d8539a8.js"
+  }
+]);
